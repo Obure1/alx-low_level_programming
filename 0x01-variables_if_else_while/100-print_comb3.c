756b4092b3a394440f201f4e752ba6a18f7d865d
Obure1 <ellosoltechnologies@gmail.com>
2023-02-20 16:13:00 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,8 +9,10 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
+		for (i = '0'; i <= '9'; i++)
+		{
 		if (c < i)
 		{
 			putchar(c);
@@ -19,7 +21,7 @@ int main(void)
 			if (c != '8' || (c == '8' && i != '9'))
 			{
 				putchar(',');
-				putchar('');
+				putchar(' ');
 			}
 		}
 	}
